3bf5d4f5328f551f7194a411e74076ae3e4d8d15
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-15 23:40:30 +0200

    Remove boilerplate

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <nav className="nav--bar">
+      <ul className="nav--ul">
+        <li className="nav--list__item"></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
