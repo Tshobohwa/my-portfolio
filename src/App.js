8a94c9df16f14f1b17ef82f2b9efc38471af733d
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-28 23:34:12 +0200

    Render home and navbar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="app--header">
+        <NavBar />
+      </header>
+      <main className="app--main__section">
+        <Home />
+      </main>
+    </div>
+  );
 }
 
 export default App;
