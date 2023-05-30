c9937699d4737649906c801e10a6d3e9f2825e4b
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-30 23:34:27 +0200

    Create animations

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import background from '../../assets/images/home-background.png'
 
 const Home = () => {
-  return (
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const handleImageLoadEvent = () => {
+    setImageLoaded(true)
+  }
+const homePageComment = `I'm an enthusiast, passionate, skilled, innovative, and detail-oriented full stack web developer.`;
+  const [displayedHomePageComment, setDisplayedHomePageComment] = useState('');
+  const [homeBtnDisplayed, setHomeBtnDisplayed] = useState(false);
+
+  useEffect(() => {
+    setDisplayedHomePageComment('I')
+    const homePageCommentChars = homePageComment.split('');
+    let currentIndex = 0;
+      const timer = setInterval(() => {
+        if (currentIndex < homePageCommentChars.length-1) {
+          setDisplayedHomePageComment(prevComment => prevComment + homePageCommentChars[currentIndex]);
+          currentIndex++;
+        } else {
+          clearInterval(timer);
+          setHomeBtnDisplayed(true);
+        }
+      }, 100); 
+      return () => {
+        clearInterval(timer);
+      };
+    }, []);
+    return (
     <section id='home-section' className='home--section__wrapper'>
-      <img src={background} alt="background" className='home--background__image' />
+      <img src={background} alt="background" onLoad={handleImageLoadEvent} className={`home--background__image ${imageLoaded? '':'home--bg__hidden'}`} />
       <div className='home--section__container'>
         <div className='brand--container'>
           <h1 className='brand--title'> <span>S</span> <span>A</span> <span>L</span> <span>O</span> <span>M</span> <span>O</span> <span>N</span> </h1>
@@ -13,9 +38,9 @@ const Home = () => {
           <small className='brand--small' ><span>TSHOBOHWA</span><span>SULEMANI</span></small>
         </div>
         <div className='comments--btn__container'>
-          <p className='home--page__comment'>I'm an enthusiast, passionate, skilled, innovative, and detail-oriented <br/> full stack web developer.</p>
+          <p className='home--page__comment'> {displayedHomePageComment} </p>
           <div className='actions--buttons__container'>
-            <button className='home--btn'>Let's connect</button>
+            <button className={`home--btn ${homeBtnDisplayed? '': 'home--btn__hidden'}`}>Let's connect</button>
           </div>
         </div>
       </div>
