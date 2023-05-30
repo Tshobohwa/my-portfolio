259c637670660be82913aeedd7ae6b10b9882479
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-30 11:38:34 +0200

    Refactor about -section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,13 +4,13 @@ import profilePic from '../../assets/images/profile-pic.png'
 const About = () => {
   return (
       <section className='about--section__wrapper' id='about'>
-        <div className='about--image__container'>
-            <h2 className='about--section__title'>About me</h2>
+        <h2 className='about--section__title'>About me</h2>
+        <div className='about--section__container'>
+            <img src={profilePic} alt="SALOMON PIC" className='about--section__profile'/>
             <p className='about--section__text'>I'm a passionate web developer specialized in various technologies,
              dedicated to building web applications and crafting visually stunning websites for companies. I thrive on
               tackling challenging problems, constantly expanding my knowledge of programming languages and emerging
                technologies.</p>
-            <img src={profilePic} alt="SALOMON PIC" className='about--section__profile'/>
         </div>
       </section>
   )
