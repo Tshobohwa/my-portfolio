2408741fd2c9fd7889c690b5c64341c5d399f559
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-31 07:58:46 +0200

    Add footer btn

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,6 +12,7 @@ const Footer = () => {
             <input type="text" placeholder='Write your name here' className='footer--form__input'/>
             <input type="email" placeholder='Write your email here' className='footer--form__input'/>
             <textarea name="message" cols="30" rows="10" placeholder='Writre your message here' className='footer--form__textarea' ></textarea>
+            <button className='footer--form__btn' type='submit'>Get in touch</button>
         </form>
         <footer className='page--footer__footer'>
             <p>All right reserved</p>
